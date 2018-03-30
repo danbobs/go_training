617469444d89690c05b8242f53f9faef0a4f1c2c
Dan Poxton <danbobs27@gmail.com>
2018-03-30 15:30:14 +0100

    Added third echo variant

---
+++ 02_echo/echo3/main.go
@@ -0,0 +1,12 @@
+// Echo3 prints the command line arguments, most efficient variant
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
