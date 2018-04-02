f850fd4cf856e59f30e13a53ec855db0cb67e4a9
Dan.Poxton <danbobs27@gmail.com>
2018-04-02 21:23:56 +0100

    Concurrent version of fetch

---
+++ 05_fetch/fetch_concurrent/main.go
@@ -0,0 +1,50 @@
+// Fetch: supports concurrent url fetches
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(getFullUrl(url))
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nBytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("While reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs \t %7d \t %s", secs, nBytes, url)
+}
+
+func getFullUrl(url string) string {
+	if !strings.HasPrefix(url, "http://") {
+		url = "http://" + url
+	}
+	return url
+}
