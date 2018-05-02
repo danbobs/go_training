d75e58036d44271bbfc4240e3a236b5ee6a3cf2e
Dan.Poxton <danbobs27@gmail.com>
2018-05-02 18:58:52 +0100

    Limited subsampling for mandlebrot

--- ch3/mandlebrot/mandlebrot.go
+++ ch3/mandlebrot/mandlebrot.go
@@ -9,40 +9,53 @@ import (
 	"os"
 )
 
+const (
+	xmin, ymin, xmax, ymax = -2, -2, 2, 2
+	width, height          = 1024, 1024
+	iterations             = 200
+	contrast               = 15
+)
+
 func main() {
-	const (
-		xmin, ymin, xmax, ymax = -2, -2, 2, 2
-		width, height          = 1024, 1024
-	)
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
+		y1 := float64(py)/height*(ymax-ymin) + ymin
+		y2 := (float64(py)+0.5)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
-			img.Set(px, py, mandlebrot(z))
+			x1 := float64(px)/width*(xmax-xmin) + xmin
+			x2 := (float64(px)+0.5)/width*(xmax-xmin) + xmin
+			z1, z2, z3, z4 := complex(x1, y1), complex(x1, y2), complex(x2, y1), complex(x2, y2)
+			m1, m2, m3, m4 := mandlebrot(z1), mandlebrot(z2), mandlebrot(z3), mandlebrot(z4)
+			average := (float32(m1) + float32(m2) + float32(m3) + float32(m4)) / 4
+			col := colorFromIterations(uint8(average))
+			img.Set(px, py, col)
 		}
 	}
 	png.Encode(os.Stdout, img)
 }
 
-func mandlebrot(z complex128) color.Color {
-	const iterations = 200
-	const contrast = 15
+func mandlebrot(z complex128) uint8 {
 
 	var v complex128
 
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-
-			r := 255 - n*contrast/3
-			g := 255 - n*contrast/2
-			b := 20 + n*contrast
-			return color.RGBA{R: r, G: g, B: b, A: 255}
+			return n
 		}
 	}
+	return iterations
+}
 
-	return color.Black
+func colorFromIterations(n uint8) color.Color {
+
+	if n == iterations {
+		return color.Black
+	}
+
+	r := 255 - n*contrast/3
+	g := 255 - n*contrast/2
+	b := 20 + n*contrast
+	return color.RGBA{R: r, G: g, B: b, A: 255}
 }
