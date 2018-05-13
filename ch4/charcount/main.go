649a01110475e86304195366fe940a43b57aa62e
Dan.Poxton <danbobs27@gmail.com>
2018-05-13 19:49:11 +0100

    Added charcount

---
+++ ch4/charcount/main.go
@@ -0,0 +1,54 @@
+// charcount counts the occurences of unicode characters
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+
+	counts := make(map[rune]int)    // counts of unicode characters
+	var utfLen [utf8.UTFMax + 1]int // counts of lengths of utf8 encodings
+	invalid := 0                    // counts of invalid characters
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("charcount: %v", err)
+			os.Exit(0)
+		}
+
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+
+		counts[r]++
+		utfLen[n]++
+	}
+
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+
+	fmt.Printf("rune\tcount\n")
+	for i, n := range utfLen {
+		fmt.Printf("%d\t%d\n", i, n)
+	}
+
+	if invalid > 0 {
+		fmt.Printf("Invalid chars: %d", invalid)
+	}
+}
--- ch4/slice_confusion/main.go
+++ ch4/slice_confusion/main.go
@@ -9,10 +9,14 @@ func main() {
 	s := []int{1, 2, 3, 4, 5}
 	fmt.Printf("%v %v %v\n", s, len(s), cap(s))
 
+	// len 1 capacity 1
 	test1 := s[4:]
 	fmt.Printf("%v %v %v\n", test1, len(test1), cap(test1))
 
+	// len 0 cap 0 - not out of bounds!
 	test2 := s[5:]
 	fmt.Printf("%v %v %v\n", test2, len(test2), cap(test2))
+
+	//test3 := s[6:] index out of bounds
 
 }
