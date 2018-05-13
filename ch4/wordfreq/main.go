308d83117c2e6469c3327abdad91ce39f9e7f7e0
Dan.Poxton <danbobs27@gmail.com>
2018-05-13 21:43:46 +0100

    Added wordfreq

---
+++ ch4/wordfreq/main.go
@@ -0,0 +1,27 @@
+// charcount counts the occurences of words
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	counts := make(map[string]int) // counts of words
+
+	reader := bufio.NewReader(os.Stdin)
+	scan := bufio.NewScanner(reader)
+	scan.Split(bufio.ScanWords)
+
+	for scan.Scan() {
+		counts[scan.Text()]++
+	}
+
+	fmt.Printf("word\t\tcount\n")
+	for w, c := range counts {
+		fmt.Printf("%s\t%d\n", w, c)
+	}
+
+}
